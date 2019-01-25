0db468303b296f59b9bb7cf054692606defdd343
adrienp <adrien_perez@sfu.ca>
2019-01-24 16:52:26 -0800

    initial commit for a working build

---
+++ lib/mud/include/room.h
@@ -0,0 +1,25 @@
+//
+// Created by Alex on 1/18/2019.
+//
+
+#ifndef ROOM_H
+#define ROOM_H
+
+using namespace std;
+
+class Room {
+	private:
+		int id;
+		string name;
+		string desc;
+		//list of 'doors'
+		//list of 'extended_descriptions'
+	public:
+		/*string name;
+		string description;
+		void resetRoom();
+		int get_dummy_objects;
+		int get_dummy_characters;*/
+};
+
+//TODO: complete based on given json file
---
+++ lib/mud/include/world.h
@@ -0,0 +1,18 @@
+//
+// Created by Adrien on 1/17/2019.
+//
+
+#ifndef WORLD_H
+#define WORLD_H
+
+#include <iostream>
+
+class World {
+
+public:
+    void getMessageFromServer(std::string msg);
+    std::string getMotd();
+    World();
+};
+
+#endif //WORLD_H
