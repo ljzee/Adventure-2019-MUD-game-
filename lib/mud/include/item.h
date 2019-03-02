68ef1ef111cbe31cf5ef30aaca96dfc09fa1994c
Andrew Lim <aglim@sfu.ca>
2019-03-01 19:23:48 -0800

    Fleshed out some item classes

--- lib/mud/include/item.h
+++ lib/mud/include/item.h
@@ -5,4 +5,76 @@
 #ifndef ADVENTURE2019_ITEM_H
 #define ADVENTURE2019_ITEM_H
 
+// To-Dos: add clone functionality and behavior for each type
+
+namespace item;
+
+class baseItem {
+
+public:
+    enum itemType{CONSUMABLE, EQUIPMENT, MISC};
+
+    baseItem(int id, int type, std::string name, std::string desc);
+
+    // Getters
+    const int getID() const {return itemID;};
+    const itemType getType() const {return itemClass;};
+    const std::string getName() const {return itemName;};
+    const std::string getDesc() const {return itemDesc;};
+
+private:
+    itemType itemClass;
+
+    std::string itemName;
+    std::string itemDesc;
+
+    bool isQuestItem;
+    bool isDroppable;
+};
+
+// CONSUMABLES
+class usableItem : public baseItem {
+
+public:
+    usableItem(int );
+    const int getEffect() const {return effectID;};
+    //const std::string getModifiers() const {return modifiers;};
+private:
+    int effectID;
+    int sellValue;
+    //vector<std::string> modifiers;
+};
+
+
+
+// EQUIPMENT
+class equipItem : public baseItem {
+
+public:
+    equipItem();
+
+private:
+    int equipSlot;
+
+    int baseDef;
+    int baseAtk;
+    int effectID;
+
+    //vector<std::string> modifiers;
+    int sellValue;
+};
+
+
+// MISCELLANEOUS
+class miscItem : public baseItem {
+
+public:
+    miscItem();
+private:
+};
+
+
+
+
+
 #endif //ADVENTURE2019_ITEM_H
--- lib/mud/include/itemManager.h
+++ lib/mud/include/itemManager.h
@@ -5,4 +5,18 @@
 #ifndef ADVENTURE2019_ITEMMANAGER_H
 #define ADVENTURE2019_ITEMMANAGER_H
 
+namespace item;
+
+typedef int itemID;
+
+class itemManager {
+public:
+
+
+
+private:
+    std::unordered_map<itemID, baseItem*> itemTable;
+
+};
+
 #endif //ADVENTURE2019_ITEMMANAGER_H
