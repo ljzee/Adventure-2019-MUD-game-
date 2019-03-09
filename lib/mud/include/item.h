7da40a83c46e03835b9653c5bf01c031dc89e214
aglim <andrew_lim_2@sfu.ca>
2019-03-08 20:30:14 -0800

    added initial header for Inventory class

--- lib/mud/include/item.h
+++ lib/mud/include/item.h
@@ -5,76 +5,108 @@
 #ifndef ADVENTURE2019_ITEM_H
 #define ADVENTURE2019_ITEM_H
 
-// To-Dos: add clone functionality and behavior for each type
+#include <string>
+#include <vector>
+#include <unordered_map>
 
-namespace item;
+namespace item {
 
-class baseItem {
+    typedef int itemID;
 
-public:
-    enum itemType{CONSUMABLE, EQUIPMENT, MISC};
+    class baseItem {
 
-    baseItem(int id, int type, std::string name, std::string desc);
+    public:
+        enum itemType {
+            CONSUMABLE, EQUIPMENT, MISC
+        };
 
-    // Getters
-    const int getID() const {return itemID;};
-    const itemType getType() const {return itemClass;};
-    const std::string getName() const {return itemName;};
-    const std::string getDesc() const {return itemDesc;};
+        baseItem(int type, std::string name, std::string desc, bool quest, bool droppable);
 
-private:
-    itemType itemClass;
+        // Getters
+        const itemType getType() const { return itemClass; };
 
-    std::string itemName;
-    std::string itemDesc;
+        const std::string getName() const { return itemName; };
 
-    bool isQuestItem;
-    bool isDroppable;
-};
+        const std::string getDesc() const { return itemDesc; };
+
+    private:
+        itemType itemClass;
+
+        std::string itemName;
+        std::string itemDesc;
+
+        bool isQuestItem;
+        bool isDroppable;
+    };
 
 // CONSUMABLES
-class usableItem : public baseItem {
+    class usableItem : public baseItem {
 
-public:
-    usableItem(int );
-    const int getEffect() const {return effectID;};
-    //const std::string getModifiers() const {return modifiers;};
-private:
-    int effectID;
-    int sellValue;
-    //vector<std::string> modifiers;
-};
+    public:
+        usableItem(int);
 
+        const int getEffect() const { return effectID; };
+        //const std::string getModifiers() const {return modifiers;};
+    private:
+        int effectID;
+        int sellValue;
+        //vector<std::string> modifiers;
+    };
 
 
 // EQUIPMENT
-class equipItem : public baseItem {
+    class equipItem : public baseItem {
 
-public:
-    equipItem();
+    public:
+        equipItem();
 
-private:
-    int equipSlot;
+    private:
+        int equipSlot;
 
-    int baseDef;
-    int baseAtk;
-    int effectID;
+        int baseDef;
+        int baseAtk;
+        int effectID;
 
-    //vector<std::string> modifiers;
-    int sellValue;
-};
+        //vector<std::string> modifiers;
+        int sellValue;
+    };
 
 
 // MISCELLANEOUS
-class miscItem : public baseItem {
+    class miscItem : public baseItem {
 
-public:
-    miscItem();
-private:
-};
+    public:
+        miscItem();
+
+    private:
+    };
+
+    class Inventory {
+    public:
+        static const int maxQuant = 99;
+
+        Inventory(int maxSize);
+
+        const bool checkOwnItem(int itemID) const;
+        const bool checkItemQuantity(int itemID) const;
+        const int checkInvSize() {return invSize;};
+        const std::string listItems() const;
+
+        bool addItem(int itemID, int quantity);
+        bool removeItem(int itemID, int Quantity);
+
+        bool alterInvSize(int newSize);
 
 
 
+    private:
+        // invContainer maps an item's itemID to its quantity within the inventory.
+        std::unordered_map<itemID, int> invContainer;
+        int invSize = 10;
 
+    };
+
+
+}
 
 #endif //ADVENTURE2019_ITEM_H
--- lib/mud/src/item.cpp
+++ lib/mud/src/item.cpp
@@ -2,3 +2,7 @@
 // Created by Andrew on 2019-02-19.
 //
 
+#include "item.h"
+
+#include <iostream>
+#include <string>
